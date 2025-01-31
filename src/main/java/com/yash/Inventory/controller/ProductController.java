d25aa2efec164f769ae51f82f26d37371cca7b89
Aadarsh  Raghuvanshi <aadarsh.raghuvanshi@Yash.com>
2025-01-31 15:12:45 +0530

    final deployment

--- src/main/java/com/yash/Inventory/controller/ProductController.java
+++ src/main/java/com/yash/Inventory/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.yash.Inventory.model.Product;
+//import com.yash.Inventory.service.ProductService;
 import com.yash.Inventory.service.ProductService;
 
 import jakarta.validation.Valid;
@@ -51,71 +52,331 @@ public class ProductController {
             productService.deleteProduct(id);
             return ResponseEntity.status(HttpStatus.OK).body("Product Deleted Successfully");
     }
-    @GetMapping("/userlist")
-    public String getUserList1() {
-		return"[\n" +
+    @GetMapping("/user1")
+    public String getUser1(@PathVariable int id) {
+		return"{\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"John Doe\",\n" +
+                "    \"phone\": \"123-456-7890\",\n" +
+                "    \"email\": \"john.doe@example.com\"\n" +
+                "}\n";
+
+    }
+    @GetMapping("/user2")
+    public String getUser2(@PathVariable int id) {
+		return"{\n" +
+                "    \"id\": 2,\n" +
+                "    \"name\": \"Ashu\",\n" +
+                "    \"phone\": \"123-456-7890\",\n" +
+                "    \"email\": \"ashu.doe@yash.com\"\n" +
+                "}\n";
+    }
+
+    @GetMapping("/user3")
+    public String getUser3() {
+        return"[\n" +
                 "    {\n" +
                 "        \"id\": 1,\n" +
                 "        \"name\": \"John Doe\",\n" +
-                "        \"age\": 30,\n" +
-                "        \"email\": \"john.doe@example.com\",\n" +
                 "        \"phone\": \"123-456-7890\",\n" +
-                "        \"address\": {\n" +
-                "            \"street\": \"123 Main St\",\n" +
-                "            \"city\": \"New York\",\n" +
-                "            \"state\": \"NY\",\n" +
-                "            \"zip\": \"10001\"\n" +
-                "        },\n" +
-                "        \"isActive\": true\n" +
+                "        \"email\": \"john.doe@example.com\"\n" +
                 "    },\n" +
                 "    {\n" +
                 "        \"id\": 2,\n" +
                 "        \"name\": \"Jane Smith\",\n" +
-                "        \"age\": 28,\n" +
-                "        \"email\": \"jane.smith@example.com\",\n" +
                 "        \"phone\": \"987-654-3210\",\n" +
-                "        \"address\": {\n" +
-                "            \"street\": \"456 Elm St\",\n" +
-                "            \"city\": \"Los Angeles\",\n" +
-                "            \"state\": \"CA\",\n" +
-                "            \"zip\": \"90001\"\n" +
-                "        },\n" +
-                "        \"isActive\": false\n" +
+                "        \"email\": \"jane.smith@example.com\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Alice Johnson\",\n" +
+                "        \"phone\": \"555-123-4567\",\n" +
+                "        \"email\": \"alice.johnson@example.com\"\n" +
                 "    }\n" +
-                "]";
-
+                "]\n";
     }
-    @GetMapping("/userlist1")
-    public String getUserList2() {
-		return"[\n" +
+
+    @GetMapping("/user4")
+    public String getUser4() {
+        return"[\n" +
                 "    {\n" +
                 "        \"id\": 1,\n" +
-                "        \"name\": \"Ashu\",\n" +
-                "        \"age\": 30,\n" +
-                "        \"email\": \"john.doe@yash.com\",\n" +
-                "        \"phone\": \"123-456-7899\",\n" +
-                "        \"address\": {\n" +
-                "            \"street\": \"124 Main St\",\n" +
+                "        \"name\": \"John Doe\",\n" +
+                "        \"phone\": \"123-456-7890\",\n" +
+                "        \"email\": \"ashu.doe@example.com\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"name\": \"Jane Smith KK\",\n" +
+                "        \"phone\": \"987-654-3211\",\n" +
+                "        \"email\": \"jane.smith@example.comm\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Johnson\",\n" +
+                "        \"phone\": \"555-123-4567\",\n" +
+                "        \"email\": \"alice.johnson@example.com\"\n" +
+                "    }\n" +
+                "]\n";
+    }
+
+    @GetMapping("/user5")
+    public String getUser5(@PathVariable int id) {
+        return"{\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"John Doe\",\n" +
+                "    \"address\": {\n" +
+                "        \"city\": \"New York\",\n" +
+                "        \"state\": \"NY\"\n" +
+                "    }\n" +
+                "}\n";
+    }
+
+    @GetMapping("/user6")
+    public String getUser6(@PathVariable int id) {
+        return"{\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"Ashutosh\",\n" +
+                "    \"address\": {\n" +
+                "        \"city\": \"INDIA\",\n" +
+                "        \"state\": \"IND\"\n" +
+                "    }\n" +
+                "}\n";
+    }
+
+    @GetMapping("/user7")
+    public String getUser7(@PathVariable int id) {
+        return"{\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"John Doe\",\n" +
+                "    \"address\": [\n" +
+                "        {\n" +
                 "            \"city\": \"New York\",\n" +
-                "            \"state\": \"Ind\",\n" +
-                "            \"zip\": \"10001\"\n" +
+                "            \"state\": \"NY\"\n" +
                 "        },\n" +
-                "        \"isActive\": true\n" +
+                "        {\n" +
+                "            \"city\": \"Los Angeles\",\n" +
+                "            \"state\": \"CA\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n";
+    }
+
+    @GetMapping("/user8")
+    public String getUser8(@PathVariable int id) {
+        return"{\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"John De\",\n" +
+                "    \"address\": [\n" +
+                "        {\n" +
+                "            \"city\": \"New ork\",\n" +
+                "            \"state\": \"NY\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"city\": \"Los Angeles\",\n" +
+                "            \"state\": \"CA\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}\n";
+    }
+
+    @GetMapping("/user9")
+    public String getUser9() {
+        return"[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"John Doe\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"New York\",\n" +
+                "            \"state\": \"NY\"\n" +
+                "        }\n" +
                 "    },\n" +
                 "    {\n" +
                 "        \"id\": 2,\n" +
                 "        \"name\": \"Jane Smith\",\n" +
-                "        \"age\": 38,\n" +
-                "        \"email\": \"ashu.smith@example.com\",\n" +
-                "        \"phone\": \"987-654-3210\",\n" +
-                "        \"addres\": {\n" +
-                "            \"street\": \"456 Elm St\",\n" +
+                "        \"address\": {\n" +
                 "            \"city\": \"Los Angeles\",\n" +
-                "            \"state\": \"CA\",\n" +
-                "            \"zip\": \"90081\"\n" +
-                "        },\n" +
-                "        \"isActive\": true\n" +
+                "            \"state\": \"CA\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Alice Johnson\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"Chicago\",\n" +
+                "            \"state\": \"IL\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 4,\n" +
+                "        \"name\": \"Bob Brown\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"Houston\",\n" +
+                "            \"state\": \"TX\"\n" +
+                "        }\n" +
                 "    }\n" +
-                "]";
+                "]\n";
+    }
+
+    @GetMapping("/user10")
+    public String getUser10() {
+        return"[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"John DoSe\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"New Yk\",\n" +
+                "            \"state\": \"NY\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"name\": \"Jane S\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"Los Angeles\",\n" +
+                "            \"state\": \"CA\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Alice JohSnson\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"Chicago\",\n" +
+                "            \"state\": \"IL\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 4,\n" +
+                "        \"name\": \"Bob S\",\n" +
+                "        \"address\": {\n" +
+                "            \"city\": \"Houston\",\n" +
+                "            \"state\": \"TX\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "]\n";
+    }
+
+    @GetMapping("/user11")
+    public String getUser11() {
+        return"[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"John Doe\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"New York\",\n" +
+                "                \"state\": \"NY\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Los Angeles\",\n" +
+                "                \"state\": \"CA\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"name\": \"Jane Smith\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"Chicago\",\n" +
+                "                \"state\": \"IL\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Houston\",\n" +
+                "                \"state\": \"TX\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Alice Johnson\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"Phoenix\",\n" +
+                "                \"state\": \"AZ\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Philadelphia\",\n" +
+                "                \"state\": \"PA\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 4,\n" +
+                "        \"name\": \"Bob Brown\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"San Antonio\",\n" +
+                "                \"state\": \"TX\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"San Diego\",\n" +
+                "                \"state\": \"CA\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "]\n";
+    }
+
+    @GetMapping("/user12")
+    public String getUser12() {
+        return"[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"name\": \"Aditya Doe\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"Indore\",\n" +
+                "                \"state\": \"indo\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Ujjain\",\n" +
+                "                \"state\": \"uJj\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"name\": \"Jth\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"dsad\",\n" +
+                "                \"state\": \"IL\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Houston\",\n" +
+                "                \"state\": \"TX\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Alice Johson\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"Phoeni\",\n" +
+                "                \"state\": \"AZ\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"Philadelphia\",\n" +
+                "                \"state\": \"P\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 4,\n" +
+                "        \"name\": \"Bb Brown\",\n" +
+                "        \"address\": [\n" +
+                "            {\n" +
+                "                \"city\": \"San Antonio\",\n" +
+                "                \"state\": \"TX\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "                \"city\": \"San Diego\",\n" +
+                "                \"state\": \"TA\"\n" +
+                "            }\n" +
+                "        ]\n" +
+                "    }\n" +
+                "]\n";
     }
 }
